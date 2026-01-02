910f96b8e1d0c0093ea832614e5f9ddadb027255
User8885 <takodajhorton@protonmail.com>
2026-01-01 23:18:18 -0500

    Big Big Small?

--- .config/waybar/scripts/custom-swaync.sh
+++ .config/waybar/scripts/custom-swaync.sh
@@ -3,9 +3,9 @@
 export dnd=$(swaync-client -D)
 
 if [[ "$dnd" == "true" ]]; then
-  echo "󰂛 $(swaync-client -c)"
+  echo "󰂛  $(swaync-client -c)"
 else
-  echo "󰂚 $(swaync-client -c)" 
+  echo "󰂚  $(swaync-client -c)" 
 fi
 
 exit
